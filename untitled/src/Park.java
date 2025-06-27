c6d7e4881f110bf7884f756a3ad209bde9f59250
Katerina <ekate-meshkova@yandex.ru>
2025-06-27 20:58:50 +0300

    1

--- untitled/src/Park.java
+++ untitled/src/Park.java
@@ -1,39 +1,53 @@
+
 public class Park {
     private String parkName;
     private Attraction[] attractions;
 
+    // Конструктор парка
+    public Park(String name, Attraction[] attractions) {
+        this.parkName = name;
+        this.attractions = attractions;
+    }
 
-    public class Attraction {
-        private String name;
+    // Внутренний класс для аттракционов
+    public static class Attraction {
+        private String attractionName;
         private String workingHours;
         private double price;
 
-        public Attraction(String name, String workingHours, double price) {
-            this.name = name;
-            this.workingHours = workingHours;
+        public Attraction(String name, String hours, double price) {
+            this.attractionName = name;
+            this.workingHours = hours;
             this.price = price;
         }
 
         public void printInfo() {
-            System.out.println("Аттракцион: " + name);
+            System.out.println("Аттракцион: " + attractionName);
             System.out.println("Время работы: " + workingHours);
-            System.out.println("Стоимость: $" + price);
-            System.out.println("----------------------");
+            System.out.println("Стоимость: " + price + "руб ");
+            System.out.println("-----------------------------");
         }
     }
 
-
-    public Park(String parkName, Attraction[] attractions) {
-        this.parkName = parkName;
-        this.attractions = attractions;
-    }
-
-
+    // Метод для вывода информации о парке и его аттракционах
     public void printParkInfo() {
-        System.out.println("Парк: " + parkName);
-        System.out.println("Аттракционы:");
+        System.out.println("Добро пожаловать в парк: " + parkName);
+        System.out.println("Наши аттракционы:");
         for (Attraction attraction : attractions) {
             attraction.printInfo();
         }
-    } }
+    }
 
+    // Пример использования
+    public static void main(String[] args) {
+        // Создаем аттракционы
+        Park.Attraction[] attractions = new Park.Attraction[3];
+        attractions[0] = new Park.Attraction("Американские горки", "10:00-20:00", 450.);
+        attractions[1] = new Park.Attraction("Колесо обозрения", "09:00-22:00", 250);
+        attractions[2] = new Park.Attraction("Водные горки", "11:00-19:00", 1000);
+
+        // Создаем парк
+        Park myPark = new Park("Развлечений", attractions);
+
+        // Выводим информацию о парке
+        myPark.printParkInfo();
