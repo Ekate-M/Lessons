b5ac7c0eedf869b220c6e69bb253eb8ba9dc5e61
Katerina <ekate-meshkova@yandex.ru>
2025-07-02 19:27:31 +0300

    1

---
+++ untitled/src/test/java/RestAssuredTest.java
@@ -0,0 +1,38 @@
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class RestAssuredTest {
+
+    @Test
+    public void testGetRequestWithFullValidation() {
+        given()
+                .baseUri("https://postman-echo.com")  // Базовый URL без эндпоинта
+                .queryParam("test", "example")         // Добавляем тестовый параметр
+                .when()
+                .get("/get")                          // Указываем эндпоинт отдельно
+                .then()
+                .assertThat()
+                .statusCode(200)                       // Проверка кода ответа
+                // Проверка структуры ответа
+                .body("args.test", equalTo("example")) // Проверка переданного параметра
+                .body("url", equalTo("https://postman-echo.com/get?test=example"))
+                .body("headers", hasKey("host"))       // Проверка обязательных заголовков
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("$", hasKey("args"))             // Проверка наличия поля args
+                .log().all();                          // Логирование полного ответа
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
