fd0e557b6b45f8338699c25e2ceed591c7ac82e5
Katerina <ekate-meshkova@yandex.ru>
2025-07-04 17:44:48 +0300

    2

--- untitled/src/test/java/RestAssuredTest.java
+++ untitled/src/test/java/RestAssuredTest.java
@@ -1,26 +1,23 @@
+
+
 import org.testng.annotations.Test;
+
+import java.util.Collections;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class RestAssuredTest {
 
     @Test
-    public void testGetRequestWithFullValidation() {
+    public void testGetRequestWithParams() {
         given()
-                .baseUri("https://postman-echo.com")  // Базовый URL без конечной точки
+                .baseUri("https://postman-echo.com")
                 .when()
-                .get("/get")                          // Указываем конечную точку отдельно
+                .get("/get")
                 .then()
-                .assertThat()
-                .statusCode(200);                 // Проверка кода ответа
-
+                .statusCode(200)
+                .body("args", equalTo(Collections.emptyMap()))  // Проверяем, что args пуст
+                .body("url", equalTo("https://postman-echo.com/get"));  // URL без параметров
     }
 }
-
-
-
-
-
-
-
-
