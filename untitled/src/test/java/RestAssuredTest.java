18725ee9c83593fb21450e3d471a86fe1f0e9aa4
Katerina <ekate-meshkova@yandex.ru>
2025-07-06 13:23:34 +0300

    1

--- untitled/src/test/java/RestAssuredTest.java
+++ untitled/src/test/java/RestAssuredTest.java
@@ -1,9 +1,5 @@
-
-
 import org.testng.annotations.Test;
-
 import java.util.Collections;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -20,5 +16,37 @@ public class RestAssuredTest {
                 .body("args", equalTo(Collections.emptyMap()))  // Проверяем, что args пуст
                 .body("headers.host", equalTo("postman-echo.com")) // Проверка конкретного заголовка
                 .body("url", equalTo("https://postman-echo.com/get"));  // URL без параметров
+    }
+
+    @Test
+    public void testPostRequestWithFullValidation() {
+        String requestBody = "This is expected to be sent back as part of response body.";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain")
+                .body(requestBody)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                // Проверка args (query-параметров)
+                .body("args", equalTo(Collections.emptyMap()))
+                // Проверка данных в теле ответа
+                .body("data", equalTo(requestBody))
+                // Проверка files (загруженных файлов)
+                .body("files", equalTo(Collections.emptyMap()))
+                // Проверка form (form-data)
+                .body("form", equalTo(Collections.emptyMap()))
+                // Проверка заголовков
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.content-type", containsString("text/plain"))
+                .body("headers.user-agent", notNullValue())
+                .body("headers.accept", equalTo("*/*"))
+                .body("headers.'content-length'", equalTo("58"))
+                // Проверка json (парсинг тела как JSON)
+                .body("json", nullValue())
+                // Проверка URL
+                .body("url", equalTo("https://postman-echo.com/post"));
     }
 }
