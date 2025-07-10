2dd6ac65fe8d8df1bb3249fcb57c2ee0e7f18acb
Katerina <ekate-meshkova@yandex.ru>
2025-07-10 14:02:23 +0300

    1

--- untitled/src/Cyber.java
+++ untitled/src/Cyber.java
@@ -72,14 +72,5 @@ public class Cyber {
     }
 }
 
-class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
 
-class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
+
